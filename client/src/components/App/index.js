20081ae4cf03812f6e8fffd8fe33672617c395a2
Michael Stevenson <503stevenson@gmail.com>
2024-02-19 15:15:47 -0500

    Setup routing on front end.

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import Review from './Review';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <Review />;
-}
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+import PasswordForgetPage from '../PasswordForget';
+import * as constants from '../../constants/routes'
+
+const App = () => (
+  <Router>
+    <Routes>
+    <Route path={constants.LANDING} element={<LandingPage />} />
+      <Route path={constants.SIGN_UP} element={<SignUpPage />} />
+      <Route path={constants.SIGN_IN} element={<SignInPage />} />
+      <Route path={constants.HOME} element={<HomePage />} />
+      <Route path={constants.ACCOUNT} element={<AccountPage />} />
+      <Route path={constants.ADMIN} element={<AdminPage />} />
+      <Route path={constants.PASSWORD_FORGET} element={<PasswordForgetPage />} />
+
+      {/* Default Route */}
+      <Route path="*" element={<LandingPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const LandingPage = () => {
+    return(
+        <Typography>Hello</Typography>
+    )
+}
+
+export default LandingPage;
