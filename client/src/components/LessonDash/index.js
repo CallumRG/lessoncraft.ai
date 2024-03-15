4e2d6304e42c6cbc7e071c2e4ce61b9de3ec7ea5
Michael Stevenson <503stevenson@gmail.com>
2024-03-15 13:11:33 -0400

    UI for Lesson Dashboard, fixed sprint 1 tests, made sprint 2 tests

--- client/src/components/LessonDash/index.js
+++ client/src/components/LessonDash/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Grid, Typography } from '@mui/material';
+import { tokens } from "../../theme";
+import { Grid, Typography, Box, useTheme } from '@mui/material';
 import { API_URL } from '../../config';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 function LessonDash(props) {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     const [lessons, setLessons] = useState([]);
+    const [title, setTitle] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -14,15 +18,17 @@ function LessonDash(props) {
             if (props.user?.id) {
                 let endpoint = '';
                 if (location.pathname === '/lessondash/byme') {
+                    setTitle('My Lessons');
                     endpoint = '/lessons/byme';
                 } else if (location.pathname === '/lessondash/liked') {
+                    setTitle('Starred Lessons');
                     endpoint = '/lessons/liked';
                 }
                 const response = await axios.post(`${API_URL}${endpoint}`, { user_id: props.user.id });
                 setLessons(response.data);
             }
         } catch (error) {
-        console.error('Error fetching lessons:', error);
+            console.error('Error fetching lessons:', error);
         }
     };
 
@@ -35,13 +41,22 @@ function LessonDash(props) {
     };
 
     return (
-        <div>
-            <Typography variant="h2">Lessons Dashboard</Typography>
-            <Grid container spacing={2}>
+        <div style={{ width: "80%", margin: "0 auto", marginTop: "50px", textAlign: 'center' }}> {/* Center the content horizontally */}
+            <Typography variant="h2" style={{ textAlign: 'left', marginTop: 100, marginBottom: '50px', borderBottom: `1px solid ${colors.blueAccent[100]}`, paddingBottom: 10}}>{title}</Typography>
+            <Grid container spacing={2} style={{marginTop: 50}}>
                 {lessons.map((lesson) => (
-                <Grid item key={lesson.id} xs={12} sm={6} md={4} lg={3} onClick={() => handleLessonClick(lesson.id)} style={{ cursor: 'pointer' }}>
-                    <Typography variant="subtitle1">{lesson.title}</Typography>
-                </Grid>
+                    <Link to={`/lesson/${lesson.id}`} key={lesson.id} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <Box border={1} p={2} borderRadius={4} mb={2}>
+                        <Typography variant="h3" sx={{ textDecoration: 'underline' }}>{lesson.title}</Typography>
+                        <Typography variant="h6" display="inline">Lesson Creator: </Typography>
+                        <Typography variant="caption" display="inline">{lesson.name}</Typography>
+                        <br></br>
+                        <Typography variant="h6" display="inline">Citation: </Typography>
+                        <Typography variant="caption" display="inline">{lesson.citation}</Typography>
+                        <hr></hr>
+                        <Typography paragraph={true} variant="body1">{lesson.description}</Typography>
+                    </Box>
+                    </Link>
                 ))}
             </Grid>
         </div>
